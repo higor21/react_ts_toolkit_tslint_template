8130d6044f8638912d4ae8de2dcc3ce1c07e30ae
higor21 <felype.cet.15@gmail.com>
2021-01-08 20:29:09 -0300

    adding inital project

---
+++ my-app/src/components/CompTestAbPath.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {
+  history: any;
+}
+
+const CompTestAbPath: React.FC<Props> = ({ history }) => {
+  return <div>sfasd</div>;
+};
+
+export default CompTestAbPath;
